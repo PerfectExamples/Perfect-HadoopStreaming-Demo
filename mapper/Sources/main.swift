5c67764c1d7570d5749050b14df54a2894bb01a6
Rockford Wei <rocky@perfect.org>
2017-02-08 15:52:49 -0500

    Adding comments to the source code

--- mapper/Sources/main.swift
+++ mapper/Sources/main.swift
@@ -1,10 +1,17 @@
 import CoreFoundation
 
+// line processor
 public func lineMain() -> Bool {
+
+  // read a line from input
   guard let line = readLine() else {
     return false
   }
+
+  // turn the whole line into lowercase then filter out symbols
   let filter = line.lowercased().characters.filter{ $0 >= "a" && $0 <= "z" || $0 == " " }
+
+  // print out every word with a count of `1`
   filter.split(separator: " ").forEach { raw in
     let word = String(raw)
     print("\(word)\t1")
@@ -12,7 +19,5 @@ public func lineMain() -> Bool {
   return true
 }
 
-
+// process all lines from input
 while(lineMain()){}
-
-
--- reducer/Sources/main.swift
+++ reducer/Sources/main.swift
@@ -1,15 +1,23 @@
 import CoreFoundation
 
+// save the output result
 var dic: [String: Int] = [:]
 
+// line processor
 public func lineMain() -> Bool {
+
+  // read a line from input
   guard let line = readLine() else {
     return false
   }
+
+  // get the key / value pair
   let e = line.characters.split(separator: "\t")
   if e.count < 2 {
     return true
   }//end if
+
+  // save the result to the table
   let key = String(e[0])
   let value = Int(String(e[1])) ?? 0
   let count = dic[key] ?? 0
@@ -18,8 +26,10 @@ public func lineMain() -> Bool {
 }
 
 
+// process all liens
 while(lineMain()){}
 
+// print out the counting result.
 dic.keys.sorted().forEach { key in
   let value = dic[key] ?? 0
   print("\(key)\t\(value)")
